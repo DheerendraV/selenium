c99ab30ceea48b7a07ebeaabb22f509384c18682
Dheerendra Varma Penmatsa <132650185+DheerendraV@users.noreply.github.com>
2024-05-23 21:00:56 -0500

    Update Test Case 1.py

--- Test Case 1.py
+++ Test Case 1.py
@@ -1,15 +1,17 @@
 import os
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import time
 cService = webdriver.ChromeService(executable_path='/Users/dheerendravarmapenmatsa/drivers/chromedriver-mac-arm64/chromedriver')
 driver = webdriver.Chrome(service = cService)
-driver.get('https://www.facebook.com/login/')
-
-driver.find_element(By.ID,"email").send_keys("Admin@gmail.com")
-driver.find_element(By.ID,"pass").send_keys("admin@1234")
-driver.find_element(By.ID,"loginbutton").click()
+driver.get('https://opensource-demo.orangehrmlive.com/')
+time.sleep(3)
+driver.find_element(By.XPATH,"/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input").send_keys("Admin")
+time.sleep(1)
+driver.find_element(By.XPATH,"/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input").send_keys("admin123")
+time.sleep(1)
+driver.find_element(By.XPATH,"/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button").click()
+time.sleep(1)
 actual_title = driver.title
 expected_title = "OrangeHRM"
 if actual_title == expected_title:
